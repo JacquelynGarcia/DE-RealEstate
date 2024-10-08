648e793f622cd44121ca430229857a4fff12f40d
Jacquelyn Garcia <jack.estrada124@gmail.com>
2024-10-08 11:48:52 -0700

    beginning code

--- Scraping.py
+++ Scraping.py
@@ -1,1 +1,17 @@
-#todo
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = 'https://www.zillow.com/'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# Extract property details
+properties = []
+for listing in soup.find_all('div', class_='property-listing'):
+    name = listing.find('h2').text
+    price = listing.find('span', class_='price').text
+    location = listing.find('span', class_='location').text
+    properties.append([name, price, location])
+
+df = pd.DataFrame(properties, columns=['Name', 'Price', 'Location'])
